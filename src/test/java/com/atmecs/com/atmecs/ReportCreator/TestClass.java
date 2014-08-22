e3a7218c36a1b6ac6fecf930a86e6e9dd2a88141
Chetan <chetan@ATMECSINDT-049.(none)>
2014-08-22 20:26:48 +0530

    initial commit due to assertion

--- src/test/java/com/atmecs/com/atmecs/ReportCreator/TestClass.java
+++ src/test/java/com/atmecs/com/atmecs/ReportCreator/TestClass.java
@@ -10,6 +10,100 @@ import org.testng.annotations.Test;
 public class TestClass {
 
 	@Test
+	public void test11() {
+		System.out.println("Inside test one");
+	}
+	
+	@Test
+	public void test21()
+	{
+		System.out.println("Inside test two");
+	}
+	
+	@Test
+	public void test31()
+	{
+		System.out.println("Inside test three");
+		Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test41()
+	{
+		System.out.println("Inside test four");
+	}
+	@Test
+	public void test51() {
+		System.out.println("Inside test five");
+	}
+	
+	@Test
+	public void test61()
+	{
+		System.out.println("Inside test six");
+		//Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test71()
+	{
+		System.out.println("Inside test seven");
+		Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test81()
+	{
+		System.out.println("Inside test eight");
+	}
+	@Test
+	public void test12() {
+		System.out.println("Inside test one");
+	}
+	
+	@Test
+	public void test22()
+	{
+		System.out.println("Inside test two");
+	}
+	
+	@Test
+	public void test32()
+	{
+		System.out.println("Inside test three");
+		Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test42()
+	{
+		System.out.println("Inside test four");
+	}
+	@Test
+	public void test52() {
+		System.out.println("Inside test five");
+	}
+	
+	@Test
+	public void test62()
+	{
+		System.out.println("Inside test six");
+		//Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test72()
+	{
+		System.out.println("Inside test seven");
+		Assert.assertEquals("1", "0");
+	}
+	
+	@Test
+	public void test82()
+	{
+		System.out.println("Inside test eight");
+	}
+	@Test
 	public void test1() {
 		System.out.println("Inside test one");
 	}
